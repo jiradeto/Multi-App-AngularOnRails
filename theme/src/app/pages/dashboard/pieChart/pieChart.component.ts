176366fbb447fe56ed399a432f8717c6e9220656
jiradeto <jiradet.jd@gmail.com>
2017-05-04 18:57:15 +0700

    fix piechart initiate

--- theme/src/app/pages/dashboard/pieChart/pieChart.component.ts
+++ theme/src/app/pages/dashboard/pieChart/pieChart.component.ts
@@ -10,7 +10,7 @@ import 'easy-pie-chart/dist/jquery.easypiechart.js';
   styleUrls: ['./pieChart.scss']
 })
 // TODO: move easypiechart to component
-export class PieChart  {
+export class PieChart {
 
   public charts: Array<Object>;
   private _init = false;
@@ -22,31 +22,27 @@ export class PieChart  {
   ngAfterViewInit() {
     if (!this._init) {
       this._loadPieCharts();
-      // this._updatePieCharts();
+      this._updatePieCharts();
       this._init = true;
     }
   }
 
   private _loadPieCharts() {
-
     jQuery('.chart').each(function () {
-      
-      
       let chart = jQuery(this);
-      chart.easyPieChart();
-      // chart.easyPieChart({
-      //   easing: 'easeOutBounce',
-      //   onStep: function (from, to, percent) {
-      //     jQuery(this.el).find('.percent').text(Math.round(percent));
-      //   },
-      //   barColor: jQuery(this).attr('data-rel'),
-      //   trackColor: 'rgba(0,0,0,0)',
-      //   size: 84,
-      //   scaleLength: 0,
-      //   animation: 2000,
-      //   lineWidth: 9,
-      //   lineCap: 'round',
-      // });
+      chart.easyPieChart({
+        easing: 'easeOutBounce',
+        onStep: function (from, to, percent) {
+          jQuery(this.el).find('.percent').text(Math.round(percent));
+        },
+        barColor: jQuery(this).attr('data-rel'),
+        trackColor: 'rgba(0,0,0,0)',
+        size: 84,
+        scaleLength: 0,
+        animation: 2000,
+        lineWidth: 9,
+        lineCap: 'round',
+      });
     });
   }
 
